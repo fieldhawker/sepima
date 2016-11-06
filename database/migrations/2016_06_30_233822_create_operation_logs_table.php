7009be628432369dd7c9f4b6b641618000325c83
fieldhawker <fieldhawker@gmail.com>
2016-11-06 22:53:50 +0900

    新規作成

---
+++ database/migrations/2016_06_30_233822_create_operation_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOperationLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operation_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('screen_number');
+            $table->integer('target_id');
+            $table->integer('operator');
+            $table->text('comment');
+            $table->timestamp('executed_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('operation_logs');
+    }
+}
