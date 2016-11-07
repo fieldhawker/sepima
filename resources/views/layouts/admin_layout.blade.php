8531c278a56a5e40d8491b751c32a03fcb7839e6
fieldhawker <fieldhawker@gmail.com>
2016-11-07 23:46:18 +0900

    no message

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -67,7 +67,7 @@
 <!-- JS -->
 
 <!-- jquery -->
-<script src={{asset("plugins/jQuery/jQuery-2.2.3.min.js")}} type="text/javascript"></script>
+<script src={{asset("plugins/jQuery/jquery-2.2.3.min.js")}} type="text/javascript"></script>
 <!-- bootstrap -->
 <script src={{asset("bootstrap/js/bootstrap.min.js")}} type="text/javascript"></script>
 
