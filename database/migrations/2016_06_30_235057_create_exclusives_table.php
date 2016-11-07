862b3f485aea0a6f4839b39dd345eae840713295
fieldhawker <fieldhawker@gmail.com>
2016-11-07 22:51:25 +0900

    CURRENT_TIMESTAMPはひとつ

--- database/migrations/2016_06_30_235057_create_exclusives_table.php
+++ database/migrations/2016_06_30_235057_create_exclusives_table.php
@@ -20,7 +20,7 @@ class CreateExclusivesTable extends Migration
             $table->timestamp('expired_at')->default('0000-00-00 00:00:00');
             $table->text('comment');
             $table->timestamp('created_at')->default('0000-00-00 00:00:00');
-            $table->timestamp('updated_at')->default(DB::raw('0000-00-00 00:00:00 on update CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
